47f42040e454d5e9e17b351282005fa3446d3011
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-05-03 20:03:17 +0300

    updated aliases

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -1,13 +1,21 @@
 # Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
+#set -q XDG_DATA_HOME
+#  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+#  or set -gx OMF_PATH "$HOME/.local/share/omf"
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+  
+  ### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
 
+
+set TERM "xterm-256color"
 set EDITOR "gnome-text-editor"
 set fish_greeting
 
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
 
 ###ALIASES###
 
@@ -31,3 +39,8 @@ alias doomdoctor="~/.emacs.d/bin/doom doctor"
 alias doomupgrade="~/.emacs.d/bin/doom upgrade"
 alias doompurge="~/.emacs.d/bin/doom purge"
 alias emacs="emacsclient -c -a 'emacs'"
+
+# Changing "ls" to "exa"
+alias ls='exa -1 --color=always --group-directories-first' # my preferred listing
+
+
