ba234176f261ddfa9c13baed57b89cf32df01892
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-05-20 19:20:11 +0300

    rebuild nvim with lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,44 +1,13 @@
-require('basics')
-require('colors')
-require('telescope-config')
-require('coc-config')
-require('lualine').setup()
-
-require'nvim-treesitter.configs'.setup {
---  ensure_installed = "maintained",
-  context_commentstring = {
-    enable = true
-  },
-  highlight = {
-    enable = true,
-    disable = { "lua" }
-  },
-  indent = {
-    enable = true
-  }
-}
-
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use {'neoclide/coc.nvim', branch = 'release'}
-  use 'folke/tokyonight.nvim'
-  use 'nvim-treesitter/nvim-treesitter' 
-  use 'tpope/vim-commentary'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
-    "ur4ltz/surround.nvim",
-    config = function()
-      require"surround".setup {mappings_style = "surround"}
-    end
-  }
-  use 'maksimr/vim-jsbeautify'
-  use 'ful1e5/onedark.nvim'
-end)
+require('packer-config')
+require('colorscheme')
+require('settings')
+require('nvim-tree-config')
+require('keymap-config')
+require('lsp.language-servers')
+require('lsp.nvim-cmp')
+require('lualine-config')
+require('barbar-config')
+require('treesitter-config')
+require('lsp.null-ls')
+require('colorizer-config')
+require('autopairs-config')
